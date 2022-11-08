ac25e1db78e3e09ef04d9a65a3c861241e9803d0
Daniel <danielsheh02@gmail.com>
2022-11-08 21:23:49 +0300

    Реализовал алгоритм CYK.

--- tests/test_cfg_utils.py
+++ tests/test_cfg_utils.py
@@ -20,7 +20,7 @@ from project.cfg_utils import *
         ),
     ],
 )
-def test_cfg1(path_to_file, path_to_file_wcnf):
+def test_cfg(path_to_file, path_to_file_wcnf):
     cfg = (
         read_cfg_from_text(path_to_file)
         .eliminate_unit_productions()
@@ -29,3 +29,27 @@ def test_cfg1(path_to_file, path_to_file_wcnf):
     cfg_wcnf = to_wcnf(cfg)
     cfg_wcnf_exp = read_cfg_from_text(path_to_file_wcnf)
     assert cfg_wcnf.productions == cfg_wcnf_exp.productions
+
+
+@pytest.mark.parametrize(
+    "path_to_file, words",
+    [
+        (
+            f"{str(shared.ROOT) + os.sep}cfg_files{os.sep}cfg1",
+            ["", "ab", "z", "false"],
+        ),
+        (
+            f"{str(shared.ROOT) + os.sep}cfg_files{os.sep}cfg2",
+            ["", "fhij", "false"],
+        ),
+        (
+            f"{str(shared.ROOT) + os.sep}cfg_files{os.sep}cfg3",
+            ["", "c", "aaaacbbbb", "acb", "false"],
+        ),
+    ],
+)
+def test_cyk(path_to_file, words):
+    cfg = (read_cfg_from_text(path_to_file)).to_normal_form()
+
+    for word in words:
+        assert cfg.contains(word) == cyk(cfg, word)
