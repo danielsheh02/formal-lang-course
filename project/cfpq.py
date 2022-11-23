af6d73d118af833f6919256598d5fd4a6ffcae86
Daniel <danielsheh02@gmail.com>
2022-11-23 15:18:05 +0300

    Реализовал функцию, основанную на матричном алгоритме, решающую задачу достижимости. Протестировал написанную функциональность.

--- project/cfpq.py
+++ project/cfpq.py
@@ -1,5 +1,6 @@
 from networkx import MultiDiGraph
 from pyformlang.cfg import Variable
+from scipy.sparse import dok_array, eye
 
 from project.cfg_utils import *
 
@@ -10,16 +11,24 @@ def cfpq(
     start_nodes: set = None,
     final_nodes: set = None,
     start_symbol: Variable = Variable("S"),
+    alg_type: str = "hellings",
 ):
     if start_nodes is None:
         start_nodes = graph.nodes
     if final_nodes is None:
         final_nodes = graph.nodes
-    return {
-        (u, v)
-        for (V, u, v) in hellings(graph, cfg)
-        if V == start_symbol and u in start_nodes and v in final_nodes
-    }
+    if alg_type == "hellings":
+        return {
+            (u, v)
+            for (V, u, v) in hellings(graph, cfg)
+            if V == start_symbol and u in start_nodes and v in final_nodes
+        }
+    elif alg_type == "matrix":
+        return {
+            (u, v)
+            for (V, u, v) in matrix(graph, cfg)
+            if V == start_symbol and u in start_nodes and v in final_nodes
+        }
 
 
 def hellings(graph: MultiDiGraph, cfg: CFG):
@@ -60,3 +69,47 @@ def hellings(graph: MultiDiGraph, cfg: CFG):
                         r.append(closure)
                         m.append(closure)
     return r
+
+
+def matrix(graph: MultiDiGraph, cfg: CFG):
+    cfg = to_wcnf(cfg)
+    term_prod = set()
+    var_prod = set()
+    eps_prod = set()
+    for prod in cfg.productions:
+        if len(prod.body) == 1:
+            term_prod.add(prod)
+        elif len(prod.body) == 2:
+            var_prod.add(prod)
+        else:
+            eps_prod.add(prod)
+    adjs: dict[Variable, dok_array] = {
+        v: dok_array((len(graph.nodes), len(graph.nodes)), dtype=bool)
+        for v in cfg.variables
+    }
+    nodes = {n: i for i, n in enumerate(graph.nodes)}
+    for (v, u, label) in graph.edges(data="label"):
+        for prod in term_prod:
+            if label == prod.body[0].value:
+                adjs[prod.head][nodes[v], nodes[u]] = True
+
+    for adj in adjs.values():
+        adj.tocsr()
+    diag = eye(len(nodes), dtype=bool, format="csr")
+
+    for v in eps_prod:
+        adjs[v.head] += diag
+    changing = True
+    while changing:
+        changing = False
+        for prod in var_prod:
+            nnz_old = adjs[prod.head].nnz
+            adjs[prod.head] += adjs[prod.body[0]] @ adjs[prod.body[1]]
+            changing |= adjs[prod.head].nnz != nnz_old
+
+    nodes = {i: n for n, i in nodes.items()}
+    result = []
+    for N, adj in adjs.items():
+        for i, j in zip(*adj.nonzero()):
+            result.append((N, nodes[i], nodes[j]))
+    return result
